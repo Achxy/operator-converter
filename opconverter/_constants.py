2cf5e8f0259f460f4bbc997c7a643198af0203a9
Achxy <74144790+Achxy@users.noreply.github.com>
2022-08-05 18:56:46 +0530

    Add `CMP_SPECIAL_CASES` to `_constants`

--- opconverter/_constants.py
+++ opconverter/_constants.py
@@ -10,4 +10,13 @@ BIN_OP_SPECIAL_CASES: Final[dict[str, str]] = {
     "BitAnd": "and_",
     "MatMult": "matmul",
 }
+CMP_SPECIAL_CASES: Final[dict[str, str]] = {
+    "NotEq": "ne",
+    "LtE": "le",
+    "GtE": "ge",
+    "Is": "is_",
+    "IsNot": "is_not",
+    "In": "contains",
+    # NotIn does not have a direct implementation in stdlib `operator`
+}
 DUNDER_FUTURE_IMPORT: Final[str] = "__future__"
