60b6811871a12b4a35e63edae2d40c7db59fb4fe
Achxy <74144790+Achxy@users.noreply.github.com>
2022-08-05 18:59:10 +0530

    make `get_cmp_conversion` helper function

--- opconverter/_helpers.py
+++ opconverter/_helpers.py
@@ -1,9 +1,16 @@
-from ._constants import BIN_OP_SPECIAL_CASES
+from ._constants import BIN_OP_SPECIAL_CASES, CMP_SPECIAL_CASES
 from typing import Any
 
 
 def get_bin_conversion(op_name: str) -> str:
     return BIN_OP_SPECIAL_CASES.get(op_name, op_name.lower())
+
+
+def get_cmp_conversion(cmp_name: str) -> str:
+    if cmp_name == "NotIn":
+        msg = "NotIn does not have a direct implementation in stdlib `operator`"
+        raise ValueError(msg)
+    return CMP_SPECIAL_CASES.get(cmp_name, cmp_name.lower())
 
 
 def get_cls_name_of(obj: Any) -> str:
