9569d5a618a05f2055f16009f61e7ce7124232ca
Achxy <74144790+Achxy@users.noreply.github.com>
2022-08-06 19:44:51 +0530

    make classes be mentioned in `__all__`

--- opconverter/_ast_parse/__init__.py
+++ opconverter/_ast_parse/__init__.py
@@ -2,3 +2,12 @@ from .abstract import AbstractBaseStandardOperationFunctionNodeTransformer
 from .attr import AttributeManipulationTransformer
 from .operators import OperationNodeTransformer
 from .parser import StandardOperationFunctionNodeTransformer
+from ..typeshack import All
+
+
+__all__: All = (
+    "AbstractBaseStandardOperationFunctionNodeTransformer",
+    "AttributeManipulationTransformer",
+    "OperationNodeTransformer",
+    "StandardOperationFunctionNodeTransformer",
+)
