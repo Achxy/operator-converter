895f32586fd4dff861029cba6146899f6855881c
Achxy <74144790+Achxy@users.noreply.github.com>
2022-08-06 19:11:40 +0530

    adjust renaming of methods

--- opconverter/_ast_parse/operators.py
+++ opconverter/_ast_parse/operators.py
@@ -1,17 +1,21 @@
 from .abstract import AbstractStandardOperationFunctionTransformer
 from ast import AugAssign, Name, Assign, BinOp, Compare, Call, Load, BoolOp, And
 from ._import_helper import add_ImportFromNode
-from ._helpers import get_bin_conversion, get_cls_name_of, get_cmp_conversion
+from ._helpers import (
+    get_bin_conversion,
+    get_cls_name_of,
+    get_cmp_conversion,
+    recursively_convert_inner_nodes,
+)
 from ._constants import AUGMENT
 
-# TODO: :)
-TODO = """
+
 class OperationNodeTransformer(AbstractStandardOperationFunctionTransformer):
     def visit_AugAssign(self, node: AugAssign) -> Assign:
         op_name = AUGMENT + get_bin_conversion(get_cls_name_of(node.op))
         left, right = node.target, node.value
-        self._symbol_update(op_name)
-        new_node = OperationNodeTransformer(
+        self._extend_import_symbols(op_name)
+        converted = recursively_convert_inner_nodes(
             Assign(
                 targets=[node.target],
                 value=Call(
@@ -21,29 +25,29 @@ class OperationNodeTransformer(AbstractStandardOperationFunctionTransformer):
                 ),
             )
         )
-        self._symbol_update(*new_node.operator_import_symbols)
-        return new_node.result
+        self._extend_import_symbols(*converted.operator_import_symbols)
+        return converted.result
 
     def visit_BinOp(self, node: BinOp) -> Call:
         left, right = node.left, node.right
         op_name = get_bin_conversion(get_cls_name_of(node.op))
-        self._symbol_update(op_name)
-        new_node = OperationNodeTransformer(
+        self._extend_import_symbols(op_name)
+        converted = recursively_convert_inner_nodes(
             Call(
                 func=Name(id=op_name, ctx=Load()),
                 args=[left, right],
                 keywords=[],
             )
         )
-        self._symbol_update(*new_node.operator_import_symbols)
-        return new_node.result
+        self._extend_import_symbols(*converted.operator_import_symbols)
+        return converted.result
 
     def visit_Compare(self, node: Compare) -> Call:
         flat = [node.left] + node.comparators
         new_comparators = []
         for current, next, cmp_ops in zip(flat, flat[1:], node.ops):
             cmp_name = get_cmp_conversion(get_cls_name_of(cmp_ops))
-            self._symbol_update(cmp_name)
+            self._extend_import_symbols(cmp_name)
             new_comparators.append(
                 Call(
                     func=Name(id=cmp_name, ctx=Load()),
@@ -51,7 +55,8 @@ class OperationNodeTransformer(AbstractStandardOperationFunctionTransformer):
                     keywords=[],
                 )
             )
-        new_node = OperationNodeTransformer(BoolOp(op=And(), values=new_comparators))
-        self._symbol_update(*new_node.operator_import_symbols)
-        return new_node.result
-"""
+        converted = recursively_convert_inner_nodes(
+            BoolOp(op=And(), values=new_comparators)
+        )
+        self._extend_import_symbols(*converted.operator_import_symbols)
+        return converted.result
