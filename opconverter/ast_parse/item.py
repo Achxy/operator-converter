76588a837587fa84c7bde1c3fac1f5ecf5775c07
Achxy <74144790+Achxy@users.noreply.github.com>
2022-08-10 23:20:37 +0530

    implement `getitem` and `setitem`

--- opconverter/ast_parse/item.py
+++ opconverter/ast_parse/item.py
@@ -1,5 +1,30 @@
 from .abstract import AbstractBaseStandardOperationFunctionNodeTransformer
+from ast import Call, Del, Load, Name, Store, Subscript, Assign
 
 
 class ItemManipulationTransformer(AbstractBaseStandardOperationFunctionNodeTransformer):
     __slots__ = ()
+
+    def visit_Subscript(self, node: Subscript):
+        ctx = node.ctx
+        if isinstance(ctx, Load):
+            return self._get_subscript(node)
+        elif isinstance(ctx, Del):
+            return self._del_subscript(node)
+
+    def _get_subscript(self, node: Subscript):
+        return Call(
+            func=Name(id="getitem", ctx=Load()),
+            args=[node.value, node.slice],
+            keywords=[],
+        )
+
+    def _set_subscript(self, obj, target, value):  # ambiguous
+        pass
+
+    def _del_subscript(self, node):
+        return Call(
+            func=Name(id="delitem", ctx=Load()),
+            args=[node.value, node.slice],
+            keywords=[],
+        )
